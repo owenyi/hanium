77e18e8ef263aeceb829779e64187b84113c8dda
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2020-09-22 23:34:21 +0900

    routes/recipe/~.js 생성

---
+++ routes/recipe/recipeRoute.js
@@ -0,0 +1,16 @@
+//module dependencies
+const express = require('express');
+const router = express.Router();
+const recipe = require('./recipe.js');
+
+router.get('/getRecipe', recipe.getRecipe);
+
+router.get('/getIngredientsInRecipe', recipe.getIngredientsInRecipe);
+
+router.get('/getRecommendedRecipe', recipe.getRecommendedRecipe);
+
+router.post('/postRecipe', recipe.postRecipe);
+
+router.get('/searchRecipe', recipe.searchRecipe);
+
+module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -9,3 +9,7 @@ router.use('/ingredients', ingredientsRoute);
 //user
 const userRoute = require('./user/userRoute.js');
 router.use('/user', userRoute);
+
+//recipe
+const recipeRoute = require('./recipe/recipeRoute.js');
+router.use('/recipe', recipeRoute);
