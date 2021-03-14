a3748b1a6850d07635caf62e5d932f7d35679f33
owenyi <67588446+owenyi@users.noreply.github.com>
2021-03-14 15:50:41 +0900

    Update dbConnection.js

--- routes/config/dbConnection.js
+++ routes/config/dbConnection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : '1q2w3e4r!!',
-    database : 'footdb',
+    password : 'password',
+    database : 'db',
     multipleStatements : true
   });
   db.connect(function(err) {
